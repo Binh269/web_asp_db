b994fda5f1ee326fce4150edb88c53f1306678b4
Binh269 <147959501+Binh269@users.noreply.github.com>
2024-10-01 09:27:19 +0700

    thêm winfont tạo capcha thủ công

---
+++ login_app/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace login_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ptb_img = new System.Windows.Forms.PictureBox();
+            this.bt_run = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập text :";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input.Location = new System.Drawing.Point(150, 33);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(307, 53);
+            this.txt_input.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bt_run);
+            this.groupBox1.Controls.Add(this.ptb_img);
+            this.groupBox1.Controls.Add(this.txt_input);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ptb_img
+            // 
+            this.ptb_img.Location = new System.Drawing.Point(297, 119);
+            this.ptb_img.Name = "ptb_img";
+            this.ptb_img.Size = new System.Drawing.Size(461, 291);
+            this.ptb_img.TabIndex = 2;
+            this.ptb_img.TabStop = false;
+            // 
+            // bt_run
+            // 
+            this.bt_run.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_run.Location = new System.Drawing.Point(53, 128);
+            this.bt_run.Name = "bt_run";
+            this.bt_run.Size = new System.Drawing.Size(185, 55);
+            this.bt_run.TabIndex = 3;
+            this.bt_run.Text = "Tạo Capcha";
+            this.bt_run.UseVisualStyleBackColor = true;
+            this.bt_run.Click += new System.EventHandler(this.bt_run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_img)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox ptb_img;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button bt_run;
+    }
+}
+
---
+++ login_app/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Capcha;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace login_app
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void bt_run_Click(object sender, EventArgs e)
+        {
+            GenerateNewCaptcha();
+        }
+
+        private void GenerateNewCaptcha()
+        {
+            string currentCapchaText = txt_input.Text;
+
+            string[] imageFiles = Directory.GetFiles("E:\\C#\\NET\\web_simple\\web_apt_db\\web_api\\img\\", "*.jpg");
+            if (imageFiles.Length == 0)
+                throw new FileNotFoundException("No background images found in img folder.");
+
+            string selectedImagePath = imageFiles[random.Next(imageFiles.Length)];
+            Bitmap bitmap = new Bitmap(selectedImagePath);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            FontFamily[] fontFamilies;
+            using (InstalledFontCollection fonts = new InstalledFontCollection())
+            {
+                fontFamilies = fonts.Families;
+            }
+
+            for (int i = 0; i < currentCapchaText.Length; i++)
+            {
+                Font font = new Font("Times New Roman", 25, FontStyle.Bold); 
+                float angle = random.Next(-10, 10);
+
+                PointF point = new PointF(i * 25 + random.Next(5), random.Next(5, 15));
+
+                g.TranslateTransform(point.X, point.Y);
+                g.RotateTransform(angle);
+                g.DrawString(currentCapchaText[i].ToString(), font, Brushes.Black, new PointF(0, 0));
+                g.RotateTransform(-angle);
+                g.TranslateTransform(-point.X, -point.Y);
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                int x = random.Next(bitmap.Width);
+                int y = random.Next(bitmap.Height);
+                g.FillEllipse(Brushes.Gray, x, y, 5, 5);
+            }
+
+            g.Dispose(); 
+
+            using (var ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                ptb_img.Image = new Bitmap(ms);
+            }
+
+            bitmap.Dispose(); 
+        }
+    }
+}
